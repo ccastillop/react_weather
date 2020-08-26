b8ef069ab8b2cf5993a2f7eb2b4cedd6769cb3ac
Cristiam Castillo <ccastillo@able.co>
2020-08-26 14:57:08 -0500

    Updates

---
+++ precache-manifest.be1511b71777f81ab0ece61b7afbde1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34212f4a033d1a85ede71634b3427a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "920f7c5b6fffe3b1d30c",
+    "url": "/static/css/main.328f601f.chunk.css"
+  },
+  {
+    "revision": "514ae3c554f8f7879f25",
+    "url": "/static/js/2.2bfdb317.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2bfdb317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920f7c5b6fffe3b1d30c",
+    "url": "/static/js/main.66324070.chunk.js"
+  },
+  {
+    "revision": "79e76fcaa52a05693c1c",
+    "url": "/static/js/runtime-main.1f6e9381.js"
+  }
+]);
